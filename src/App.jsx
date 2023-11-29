8ca15c388c86a3077fe26ea22b069d0a7fe6f94d
Shawn <shawnryannacario@gmail.com>
2023-11-30 07:14:06 +0800

    Nov30 update

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,5 @@
 
 
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import NavBar from "./Component/Nav_Bar";
-import Footer from "./Component/Footer";
-import Homepage from "./Pages/Home_Page";
-import AboutUs from "./Pages/About_Us";
-import DentistsPage from "./Pages/Dentists";
-import ServicePage from "./Pages/OurServices";
-import LoginPage from "./Pages/LoginPage";
-import ProfilePage from "./Pages/CustomerProfile";
-
-import "./App.css";
-
-function App() {
-  return (
-    <div className="AppJSX-container">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route
-            path="/"
-            element={
-              <>
-                <NavBar />
-                <Routes>
-                  <Route path="/home" element={<Homepage />} />
-                  <Route path="/about" element={<AboutUs />} />
-                  <Route path="/dentists" element={<DentistsPage />} />
-                  <Route path="/service" element={<ServicePage />} />
-                  <Route path="/profile" element={<ProfilePage />} />
-                </Routes>
-                <Footer />
-              </>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-}
-
-export default App;
-
-
-
-
-
-
 // import React from "react";
 // import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import NavBar from "./Component/Nav_Bar";
@@ -59,29 +11,77 @@ export default App;
 // import LoginPage from "./Pages/LoginPage";
 // import ProfilePage from "./Pages/CustomerProfile";
 
-
-
 // import "./App.css";
-
-
 
 // function App() {
 //   return (
 //     <div className="AppJSX-container">
 //       <BrowserRouter>
-//         <NavBar />
 //         <Routes>
 //           <Route path="/" element={<LoginPage />} />
-//           <Route path="/about" element={<AboutUs />} />
-//           <Route path="/dentists" element={<DentistsPage />} />
-//           <Route path="/service" element={<ServicePage />} />
-//           <Route path="/profile" element={<ProfilePage />} /> 
+//           <Route
+//             path="/"
+//             element={
+//               <>
+//                 <NavBar />
+//                 <Routes>
+//                   <Route path="/home" element={<Homepage />} />
+//                   <Route path="/about" element={<AboutUs />} />
+//                   <Route path="/dentists" element={<DentistsPage />} />
+//                   <Route path="/service" element={<ServicePage />} />
+//                   <Route path="/profile" element={<ProfilePage />} />
+//                 </Routes>
+//                 <Footer />
+//               </>
+//             }
+//           />
 //         </Routes>
-//         <Footer />
 //       </BrowserRouter>
 //     </div>
 //   );
 // }
 
 // export default App;
+
+
+
+
+// AYAW NI HILABTA
+
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "./Component/Nav_Bar";
+import Footer from "./Component/Footer";
+import Homepage from "./Pages/Home_Page";
+import AboutUs from "./Pages/About_Us";
+import DentistsPage from "./Pages/Dentists";
+import ServicePage from "./Pages/OurServices";
+import LoginPage from "./Pages/LoginPage";
+import ProfilePage from "./Pages/CustomerProfile";
+
+
+
+import "./App.css";
+
+
+
+function App() {
+  return (
+    <div className="AppJSX-container">
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/dentists" element={<DentistsPage />} />
+          <Route path="/service" element={<ServicePage />} />
+          <Route path="/profile" element={<ProfilePage />} /> 
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
 
--- src/Pages/Home_Page.jsx
+++ src/Pages/Home_Page.jsx
@@ -1,49 +1,98 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../Styles/Home_Page.css';
 
-const HomePage = () => {
+const Homepage = () => {
+  const images = [
+    'https://cdn.discordapp.com/attachments/698869917235019828/1179515281035505895/Dentist_2.jpg?ex=657a1035&is=65679b35&hm=f752e3cc628f9923c1b3f072a5b56f934cef85d17c2e47438eac5a5a22b97afe&',
+    'https://cdn.discordapp.com/attachments/698869917235019828/1179515281354268752/Dentist_3.jpg?ex=657a1035&is=65679b35&hm=462da9e6c71da59b8118564317d9c0f9ce0b21f512e908a14ed9e15a63df2aae&',
+    'https://media.discordapp.net/attachments/698869917235019828/1179515281748545587/Dentist_1.jpeg?ex=657a1035&is=65679b35&hm=3f492272c51c067cd2409217cc87c501849548ba40f07f5a4ed0bc95c930015c&=&format=webp&width=1333&height=889',
+  ];
+
+  const [currentImage, setCurrentImage] = useState(0);
+  const [transitionDuration, setTransitionDuration] = useState(1); 
+
+
+
+
+  const handlePrev = () => {
+    setCurrentImage((prevImage) => (prevImage === 0 ? images.length - 1 : prevImage - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentImage((prevImage) => (prevImage === images.length - 1 ? 0 : prevImage + 1));
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleNext();
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, [currentImage]);
+
   return (
-    
-    <div className="home-page">
-   
-      <div className="header-image">
-      </div>
-      <div className="Homepage-container">
-        <h1>About Us</h1>
-        <p>
-          Our passion at Aurora Dental is to help kids and families achieve optimal oral health. We also seek to educate patients about the importance of getting regular dental care so they can smile brighter for a lifetime. Because we think that quality dental care should be within everyone’s reach, we follow the Alberta Dental Fee Guide, charging no extra.
-        </p>
-        <h3>Planting Roots in SE Edmonton</h3>
-        <p>
-          Delighted to open Aurora Dental, a brand-new dental clinic, Dr. Rakhee Parmar, Dr. Paul Delhon, and Dr. Travis Elliott are happy to be a part of our growing community. They are looking to make a long-term investment here. “I want to take care of the smiles of friends and neighbors for decades to come,” Dr. Rakhee said.
-        </p>
-      </div>
-      <div className="boxes-container">
-        <div className="feature-box">
-          <h2>Relax in Our Cheerful Environment</h2>
+    <div className="HomePage-container">
+      {/* Top Section */}
+      <div className="homepage-carousel-container">
+        <div className="homepage-text-container">
+          <h2>Lorem Ipsum</h2>
           <p>
-            Perhaps you've been to other dental practices in the past that have felt more clinical. That’s not the case at our office. Here, patient comfort is paramount! We have large windows, allowing lots of sunlight to brighten our space. Netflix is available for your viewing pleasure while you receive dental treatment.
+            There are many variations of pas of Lorem Ipsum availab. There are many variations of pas
+            of Lorem Ipsum availab.
           </p>
         </div>
-        <div className="feature-box">
-          <h2>Helping Patients Feel Comfortable</h2>
-          <p>
-            Perhaps you had a negative experience going to the dentist as a child. We understand that dental anxiety is real. That’s why we do everything we can to put patients at ease. Our care is gentle and we offer sedation options.
-          </p>
-        </div>
-        <div className="feature-box">
-          <h2>Maintaining a Presence in the Community</h2>
-          <p>
-            As patient education is a big part of what we do, we will be holding various events in our community. These will include free checkups and presentations to local schools. We want our patients and those in the community to enjoy healthy, happy smiles!
-          </p>
+        <div className="homepage-image-container">
+          <img
+            src={images[currentImage]}
+            alt={`Image ${currentImage + 1}`}
+            className="homepage-carousel-image"
+          />
         </div>
       </div>
-
-      
-
-      
+{/* Facilities Section */}
+      <div className="facilities">
+        <div className="facilities-text-wrapper">
+          <h1 className="facilities-text">OUR SERVICES</h1>
+          <p className="facilities-text">What Facilities We Provided</p>
+        </div>
+        <div className="facility-boxes">
+          <div className="facility-box left-box">
+            <img
+              src="https://cdn.discordapp.com/attachments/698869917235019828/1179535127076945961/endo.jpg?ex=657a22b0&is=6567adb0&hm=245fc467d292f73338e45ba61bb6a4f63da2eeaa7dfebb8b39bbdf56629f4387&" // Add the URL to your image
+              alt="Orthodontist Image"
+              className="facility-image"
+            />
+            <h3>Orthodontist</h3>
+            <p>
+              Correct misalignments seamlessly with our specialized facility. Equipped with cutting-edge tools, X-ray, and 3D imaging for precise treatment plans. Our space ensures a comfortable fit for braces, retainers, and aligners. Your journey to a straighter smile starts here.
+            </p>
+          </div>
+          <div className="facility-box middle-box">
+            <img
+              src="https://cdn.discordapp.com/attachments/698869917235019828/1179534932062777465/orthodontist.jpg?ex=657a2282&is=6567ad82&hm=6268a0bbedb45677a8cb4e7d45ae34271028e0892723614035d622d9329c76e2&" // Add the URL to your image
+              alt="Periodontist Image"
+              className="facility-image"
+            />
+            <h3>Periodontist</h3>
+            <p>
+              Experience comprehensive care in our periodontal facility. Specialized tools for deep cleanings, scaling, and root planing procedures ensure optimal gum health. Our advanced equipment diagnoses and monitors gum conditions with precision. Collaborative efforts with general dentists guarantee your overall oral health. Your journey to healthy gums begins with us.
+            </p>
+          </div>
+          <div className="facility-box right-box">
+            <img
+              src="https://cdn.discordapp.com/attachments/698869917235019828/1179534643364642836/periodontist.jpg?ex=657a223d&is=6567ad3d&hm=ef014f20b0c9b23dd370919ec9bbbd5e0df03fb5611583fad2492b98a9342cc0&" // Add the URL to your image
+              alt="Endodontist Image"
+              className="facility-image"
+            />
+            <h3>Endodontist</h3>
+            <p>
+              Discover top-tier care in our specialized facility. Treatment rooms feature advanced imaging for precise root issue diagnosis. Specialized instruments ensure expert root canal treatments and procedures. Our commitment to a sterile environment safeguards against infections, providing a secure haven for delicate procedures. Entrust your root health to our expertise.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default HomePage;
+export default Homepage;
