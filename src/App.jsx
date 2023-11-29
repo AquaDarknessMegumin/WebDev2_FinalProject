6f2cc9a562e45c21ce4cae59028f030cfc0e2a44
Shawn <shawnryannacario@gmail.com>
2023-11-29 22:26:29 +0800

    Nov29 Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+
+
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./Component/Nav_Bar";
@@ -15,18 +17,71 @@ function App() {
   return (
     <div className="AppJSX-container">
       <BrowserRouter>
-        <NavBar />
         <Routes>
           <Route path="/" element={<LoginPage />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/dentists" element={<DentistsPage />} />
-          <Route path="/service" element={<ServicePage />} />
-          <Route path="/profile" element={<ProfilePage />} /> {/* Add this line */}
+          <Route
+            path="/"
+            element={
+              <>
+                <NavBar />
+                <Routes>
+                  <Route path="/home" element={<Homepage />} />
+                  <Route path="/about" element={<AboutUs />} />
+                  <Route path="/dentists" element={<DentistsPage />} />
+                  <Route path="/service" element={<ServicePage />} />
+                  <Route path="/profile" element={<ProfilePage />} />
+                </Routes>
+                <Footer />
+              </>
+            }
+          />
         </Routes>
-        <Footer />
       </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+// import React from "react";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import NavBar from "./Component/Nav_Bar";
+// import Footer from "./Component/Footer";
+// import Homepage from "./Pages/Home_Page";
+// import AboutUs from "./Pages/About_Us";
+// import DentistsPage from "./Pages/Dentists";
+// import ServicePage from "./Pages/OurServices";
+// import LoginPage from "./Pages/LoginPage";
+// import ProfilePage from "./Pages/CustomerProfile";
+
+
+
+// import "./App.css";
+
+
+
+// function App() {
+//   return (
+//     <div className="AppJSX-container">
+//       <BrowserRouter>
+//         <NavBar />
+//         <Routes>
+//           <Route path="/" element={<LoginPage />} />
+//           <Route path="/about" element={<AboutUs />} />
+//           <Route path="/dentists" element={<DentistsPage />} />
+//           <Route path="/service" element={<ServicePage />} />
+//           <Route path="/profile" element={<ProfilePage />} /> 
+//         </Routes>
+//         <Footer />
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// export default App;
+
--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,219 +1,215 @@
 import React, { useState } from 'react';
 import '../Styles/Login_Page.css';
 
-const PrivateRoute = ({ element }) => {
-    const { isLoggedIn } = useAuth();
-  
-    return isLoggedIn ? element : <Navigate to="/" />;
-  };
 const Logo = () => (
-  <img src="pictures/DentEaseNav.png" alt="Logo" style={{ width: '100px', height: 'auto' }} />
-);
-
-const LRLoginForm = ({ onSubmit, onSwitchForm, onForgotPassword, formData, onInputChange }) => (
-  <div className="LRform-container">
-      <h2 className="LRh2">Login</h2>
-      <form className="LRform" onSubmit={onSubmit}>
-          {['LRloginUsername', 'LRloginPassword'].map((id) => (
-              <input
-                  key={id}
-                  type={id.includes('Password') ? 'password' : 'text'}
-                  id={id}
-                  placeholder={id.replace('LRlogin', '')}
-                  required
-                  className="LRinput"
-                  value={formData[id]}
-                  onChange={onInputChange}
-              />
-          ))}
-          <div className="LRform-actions">
-              <div className="LRcheckbox-label">
-                  <input
-                      type="checkbox"
-                      id="LRrememberMe"
-                      checked={formData.rememberMe}
-                      onChange={() => onInputChange({ target: { id: 'LRrememberMe', type: 'checkbox', checked: !formData.rememberMe } })}
-                  />
-                  <label htmlFor="LRrememberMe">Remember me</label>
-              </div>
-              <a href="#" className="LRforgot-password LRa" onClick={onForgotPassword}>
-                  Forgot password?
-              </a>
-          </div>
-          <button type="submit" className="LRlogin-button LRbutton">
-              Login
-          </button>
-          <p>
-              Don't have an account?{' '}
-              <button className="LRswitch-form LRregister LRbutton" onClick={() => onSwitchForm('register')}>
-                  Register
-              </button>
-          </p>
-      </form>
+    <img src="dentease.png" alt="Logo" style={{ width: '100px', height: 'auto' }} />
+  );
+  
+const LoginForm = ({ onSubmit, onSwitchForm, onForgotPassword, formData, onInputChange }) => (
+  <div className="form-container">
+    <h2 className="h2">Login</h2>
+    <form className="form" onSubmit={onSubmit}>
+      {['loginUsername', 'loginPassword'].map((id) => (
+        <input
+          key={id}
+          type={id.includes('Password') ? 'password' : 'text'}
+          id={id}
+          placeholder={id.replace('login', '')}
+          required
+          className="input"
+          value={formData[id]}
+          onChange={onInputChange}
+        />
+      ))}
+      <div className="form-actions">
+        <div className="checkbox-label">
+          <input
+            type="checkbox"
+            id="rememberMe"
+        
+            checked={formData.rememberMe}
+            onChange={() => onInputChange({ target: { id: 'rememberMe', type: 'checkbox', checked: !formData.rememberMe } })}
+          />
+          <label htmlFor="rememberMe">Remember me</label>
+        </div>
+        <a href="#" className="forgot-password a" onClick={onForgotPassword}>
+          Forgot password?
+        </a>
+      </div>
+      <button type="submit" className="login-button button">
+        Login
+      </button>
+      <p>
+        Don't have an account?{' '}
+        <button className="switch-form register button" onClick={() => onSwitchForm('register')}>
+          Register
+        </button>
+      </p>
+    </form>
   </div>
 );
 
-const LRRegisterForm = ({ onSubmit, onSwitchForm, formData, onInputChange }) => (
-  <div className="LRform-container">
-      <h2 className="LRh2">Register</h2>
-      <form className="LRform" onSubmit={onSubmit}>
-          {['LRregisterUsername', 'LRregisterPhoneNumber', 'LRregisterEmail', 'LRregisterPassword'].map((id) => (
-              <input
-                  key={id}
-                  type={id.includes('Password') ? 'password' : 'text'}
-                  id={id}
-                  placeholder={id.replace('LRregister', '')}
-                  required
-                  className="LRinput"
-                  value={formData[id]}
-                  onChange={onInputChange}
-              />
-          ))}
-          <div className="LRform-actions">
-              <div className="LRcheckbox-label">
-                  <input
-                      type="checkbox"
-                      id="LRagreeTerms"
-                      checked={formData.agreeTerms}
-                      onChange={() => onInputChange({ target: { id: 'LRagreeTerms', type: 'checkbox', checked: !formData.agreeTerms } })}
-                      required
-                  />
-                  <label htmlFor="LRagreeTerms" className="LRlabel">
-                      I agree to the terms and conditions
-                  </label>
-              </div>
-          </div>
-          <button type="submit" className="LRregister-button LRbutton">
-              Register
-          </button>
-          <p>
-              Already have an account?{' '}
-              <button className="LRswitch-form LRlogin LRbutton" onClick={() => onSwitchForm('login')}>
-                  Login
-              </button>
-          </p>
-      </form>
+const RegisterForm = ({ onSubmit, onSwitchForm, formData, onInputChange }) => (
+  <div className="form-container">
+    <h2 className="h2">Register</h2>
+    <form className="form" onSubmit={onSubmit}>
+      {['registerUsername', 'registerPhoneNumber', 'registerEmail', 'registerPassword'].map((id) => (
+        <input
+          key={id}
+          type={id.includes('Password') ? 'password' : 'text'}
+          id={id}
+          placeholder={id.replace('register', '')}
+          required
+          className="input"
+          value={formData[id]}
+          onChange={onInputChange}
+        />
+      ))}
+      <div className="form-actions">
+        <div className="checkbox-label">
+          <input
+            type="checkbox"
+            id="agreeTerms"
+            checked={formData.agreeTerms}
+            onChange={() => onInputChange({ target: { id: 'agreeTerms', type: 'checkbox', checked: !formData.agreeTerms } })}
+            required
+          />
+          <label htmlFor="agreeTerms" className="label">
+            I agree to the terms and conditions
+          </label>
+        </div>
+      </div>
+      <button type="submit" className="register-button button">
+        Register
+      </button>
+      <p>
+        Already have an account?{' '}
+        <button className="switch-form login button" onClick={() => onSwitchForm('login')}>
+          Login
+        </button>
+      </p>
+    </form>
   </div>
 );
 
-const LRForgotPasswordForm = ({ onSubmit, onSwitchForm, onInputChange }) => (
-  <div className="LRform-container">
-      <h2 className="LRh2">Forgot Password</h2>
-      <form className="LRform" onSubmit={onSubmit}>
-          {['LRforgotPasswordEmail', 'LRforgotPasswordPhoneNumber'].map((id) => (
-              <input
-                  key={id}
-                  type="text"
-                  id={id}
-                  placeholder={id.replace('LRforgotPassword', '')}
-                  required
-                  className="LRinput"
-                  onChange={onInputChange}
-              />
-          ))}
-          <button type="submit" className="LRlogin-button LRbutton">
-              Submit
-          </button>
-          <p>
-              Remember your password?{' '}
-              <button className="LRswitch-form LRlogin LRbutton" onClick={() => onSwitchForm('login')}>
-                  Login
-              </button>
-          </p>
-      </form>
+const ForgotPasswordForm = ({ onSubmit, onSwitchForm, onInputChange }) => (
+  <div className="form-container">
+    <h2 className="h2">Forgot Password</h2>
+    <form className="form" onSubmit={onSubmit}>
+      {['forgotPasswordEmail', 'forgotPasswordPhoneNumber'].map((id) => (
+        <input
+          key={id}
+          type="text"
+          id={id}
+          placeholder={id.replace('forgotPassword', '')}
+          required
+          className="input"
+          onChange={onInputChange}
+        />
+      ))}
+      <button type="submit" className="login-button button">
+        Submit
+      </button>
+      <p>
+        Remember your password?{' '}
+        <button className="switch-form login button" onClick={() => onSwitchForm('login')}>
+          Login
+        </button>
+      </p>
+    </form>
   </div>
 );
 
-function LoginPage() {
+function App() {
   const [loginForm, setLoginForm] = useState(true);
   const [forgotPasswordForm, setForgotPasswordForm] = useState(false);
   const [formData, setFormData] = useState({
-      loginUsername: '',
-      loginPassword: '',
-      rememberMe: false,
-      registerUsername: '',
-      registerPhoneNumber: '',
-      registerEmail: '',
-      registerPassword: '',
-      agreeTerms: false,
-      forgotPasswordEmail: '',
-      forgotPasswordPhoneNumber: '',
+    loginUsername: '',
+    loginPassword: '',
+    rememberMe: false,
+    registerUsername: '',
+    registerPhoneNumber: '',
+    registerEmail: '',
+    registerPassword: '',
+    agreeTerms: false,
+    forgotPasswordEmail: '',
+    forgotPasswordPhoneNumber: '',
   });
 
   const handleSwitchForm = (form) => {
-      setLoginForm(form === 'login');
-      setForgotPasswordForm(form === 'forgotPassword');
+    setLoginForm(form === 'login');
+    setForgotPasswordForm(form === 'forgotPassword');
   };
 
   const handleInputChange = (e) => {
-      const { id, value, type, checked } = e.target;
+    const { id, value, type, checked } = e.target;
 
-      if (id === 'registerPhoneNumber' && isNaN(value)) {
-          return;
-      }
+    if (id === 'registerPhoneNumber' && isNaN(value)) {
+      return;
+    }
 
-      setFormData({
-          ...formData,
-          [id]: type === 'checkbox' ? checked : value,
-      });
+    setFormData({
+      ...formData,
+      [id]: type === 'checkbox' ? checked : value,
+    });
   };
 
   const handleFormSubmit = (e) => {
-      e.preventDefault();
+    e.preventDefault();
 
-      if (loginForm) {
-          console.log('Login form submitted:', formData.loginUsername, formData.loginPassword, formData.rememberMe);
-      } else if (forgotPasswordForm) {
-          console.log('Forgot Password form submitted:', formData.forgotPasswordEmail, formData.forgotPasswordPhoneNumber);
-          setForgotPasswordForm(false);
-      } else {
-          if (!formData.agreeTerms) {
-              alert('Please agree to the terms and conditions.');
-              return;
-          }
-          console.log(
-              'Register form submitted:',
-              formData.registerUsername,
-              formData.registerPhoneNumber,
-              formData.registerEmail,
-              formData.registerPassword,
-              formData.agreeTerms,
-              formData.rememberMe
-          );
+    if (loginForm) {
+      console.log('Login form submitted:', formData.loginUsername, formData.loginPassword, formData.rememberMe);
+    } else if (forgotPasswordForm) {
+      console.log('Forgot Password form submitted:', formData.forgotPasswordEmail, formData.forgotPasswordPhoneNumber);
+      setForgotPasswordForm(false);
+    } else {
+      if (!formData.agreeTerms) {
+        alert('Please agree to the terms and conditions.');
+        return;
       }
+      console.log(
+        'Register form submitted:',
+        formData.registerUsername,
+        formData.registerPhoneNumber,
+        formData.registerEmail,
+        formData.registerPassword,
+        formData.agreeTerms,
+        formData.rememberMe
+      );
+    }
   };
 
   return (
-      <div className="body">
-          <div className="container">
-              {loginForm && !forgotPasswordForm && (
-                  <LRLoginForm
-                      onSubmit={handleFormSubmit}
-                      onSwitchForm={handleSwitchForm}
-                      onForgotPassword={() => setForgotPasswordForm(true)}
-                      formData={formData}
-                      onInputChange={handleInputChange}
-                  />
-              )}
-              {!loginForm && !forgotPasswordForm && (
-                  <LRRegisterForm
-                      onSubmit={handleFormSubmit}
-                      onSwitchForm={handleSwitchForm}
-                      formData={formData}
-                      onInputChange={handleInputChange}
-                  />
-              )}
-              {forgotPasswordForm && (
-                  <LRForgotPasswordForm
-                      onSubmit={handleFormSubmit}
-                      onSwitchForm={handleSwitchForm}
-                      formData={formData}
-                      onInputChange={handleInputChange}
-                  />
-              )}
-          </div>
+    <div className="body">
+      <div className="container">
+        {loginForm && !forgotPasswordForm && (
+          <LoginForm
+            onSubmit={handleFormSubmit}
+            onSwitchForm={handleSwitchForm}
+            onForgotPassword={() => setForgotPasswordForm(true)}
+            formData={formData}
+            onInputChange={handleInputChange}
+          />
+        )}
+        {!loginForm && !forgotPasswordForm && (
+          <RegisterForm
+            onSubmit={handleFormSubmit}
+            onSwitchForm={handleSwitchForm}
+            formData={formData}
+            onInputChange={handleInputChange}
+          />
+        )}
+        {forgotPasswordForm && (
+          <ForgotPasswordForm
+            onSubmit={handleFormSubmit}
+            onSwitchForm={handleSwitchForm}
+            formData={formData}
+            onInputChange={handleInputChange}
+          />
+        )}
       </div>
+    </div>
   );
 }
 
-export default LoginPage;
+export default App;
